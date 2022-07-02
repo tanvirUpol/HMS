577efe08f4ec51573b6f1a5ba54947847768feeb
tanvir <tanvirrahmanupol@gmail.com>
2022-07-02 20:54:51 +0600

    Upol commit

--- HMS/Controllers/AdminController.cs
+++ HMS/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HMS.Models;
 
 namespace HMS.Controllers
 {
@@ -13,5 +14,6 @@ namespace HMS.Controllers
         {
             return View();
         }
+
     }
 }
--- HMS/Controllers/HomeController.cs
+++ HMS/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HMS.Models;
 
 namespace HMS.Controllers
 {
@@ -17,6 +18,26 @@ namespace HMS.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(Admin e)
+        {
+
+                HMSEntities db = new HMSEntities();
+                var st = (from s in db.Admins
+                          where s.Name.Equals(e.Name)
+                          && s.Password.Equals(e.Password)
+                          select s).SingleOrDefault();
+                if (st != null)
+                {
+                    Session["logged_user"] = st.Id;
+                    return RedirectToAction("Index", "Home");
+                }
+                TempData["msg"] = "User Does not exist";
+                return View();
+ 
         }
 
     }
