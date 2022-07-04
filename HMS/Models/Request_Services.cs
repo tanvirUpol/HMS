ffee66dc6322c0563e94b09ecc7c7df17f213f53
tanvir <tanvirrahmanupol@gmail.com>
2022-07-04 22:23:08 +0600

    upol solved changes

--- HMS/Models/Request_Services.cs
+++ HMS/Models/Request_Services.cs
@@ -15,6 +15,13 @@ namespace HMS.Models
     public partial class Request_Services
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public int Type { get; set; }
         public int member_ID { get; set; }
         public string Service_Type { get; set; }
     }
